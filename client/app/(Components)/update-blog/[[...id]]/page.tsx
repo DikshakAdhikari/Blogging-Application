95ca260504641d91c0840dc65a190dc4619a8500
hunktuck <ddikshakk@gmail.com>
2024-01-12 22:19:30 +0530

    blog view added

--- client/app/(Components)/update-blog/[[...id]]/page.tsx
+++ client/app/(Components)/update-blog/[[...id]]/page.tsx
@@ -1,10 +1,12 @@
 "use client"
-import React, { FC, useEffect, useState } from 'react'
+import { ChangeEvent, FC, FormEvent, useEffect, useState } from "react"
 import Navbar from '../../Navbar'
 import { useRouter } from 'next/router'
 import { useSearchParams } from 'next/navigation'
 import Image from 'next/image'
 import { ProfileLogo } from '@/app/Logo'
+import  Swal from 'sweetalert2'
+
 interface pageProps{
     params: {id:string}
   }
@@ -16,6 +18,9 @@ interface pageProps{
     const [title, setTitle]= useState('')
     const [description, setDescription]= useState('')
     const [image, setImage]= useState('')
+    const [error, setError]= useState('')
+
+  const [file, setFile] = useState<File | null>(null);
     
     const handleClick= async()=> {
       try{
@@ -81,16 +86,64 @@ interface pageProps{
       }
       fun()
     },[toggle])
+    const handleFileChange = (e: ChangeEvent<HTMLInputElement>) => {
+      if (e.target.files !== null && e.target.files.length > 0) {
+        const selectedFile = e.target.files[0];
+        const allowedExtensions = ["png", "jpeg", "jpg", "svg","webp"];
+        const fileExtension = selectedFile.name.split('.').pop()?.toLowerCase();
+    
+        
+        if (fileExtension && allowedExtensions.includes(fileExtension)) {
+          setFile(selectedFile);
+          setError('')
+        } else {      
+          console.error("Invalid file type. Please select a .png, .jpg, or .svg file.");
+          setError("Invalid file type. Please select a .png, .jpg, .svg or webp file.")
+          // Optionally, you can reset the file input to clear the selection
+          e.target.value = '';
+        }
+      }
+    };
+  
+    const handleSubmit = async (e:FormEvent)=> {
+      e.preventDefault()
+      const formData= new FormData()
+      if(file){
+        formData.append('file', file)
+      }
+      formData.append('title', title)
+      formData.append('description', description)
+  
+      try{
+        const res= await fetch('http://localhost:3002/blog/', {
+          method:"POST",
+          credentials: "include",
+          body:formData
+        })
+  
+        if(!res.ok){
+          throw new Error('Network problem while creating blog!');
+        }
+        const data= await res.json()
+        console.log(data);
+        
+          Swal.fire("Blog saved successfully!");
+        
+      }catch(err){
+        console.log(err);
+        
+      }  
+    }
   
     if(!blog){
       return 
     }
-  
-  
+
     return (
       <div>
       <Navbar />
-    <form
+      <form
+      onSubmit={handleSubmit}
       className=" max-w-lg mx-auto mt-8 p-8 border rounded-lg shadow-lg"
     >
       <div className="mb-4">
@@ -98,15 +151,14 @@ interface pageProps{
           Choose File (PNG, JPEG, SVG, WEBP.)
         </label>
         <input
-        value={'1704970964595-edit_FILL0_wght400_GRAD0_opsz24.svg'}
         required
           type="file"
           id="file"
           accept=".png, .jpg, .jpeg, .svg"
-         
+          onChange={handleFileChange}
           className="mt-1 p-3 bg-white border w-full rounded-md "
         />
-        {/* {error!="" &&  <div className=" text-red-600 font-medium">{error}</div> } */}
+        {error!="" &&  <div className=" text-red-600 font-medium">{error}</div> }
       </div>
       <div className="mb-4">
         <label htmlFor="title" className="block text-sm font-medium text-gray-700">
@@ -143,8 +195,10 @@ interface pageProps{
         Submit
       </button>
     </form>
+ 
     </div>
      
     )
   }
+
   export default page
