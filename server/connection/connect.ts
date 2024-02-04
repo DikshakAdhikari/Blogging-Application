cf35c923c7176e52178b5a3437c1df2bfd20dcc0
hunktuck <ddikshakk@gmail.com>
2024-02-04 19:49:16 +0530

    mongo put

--- server/connection/connect.ts
+++ server/connection/connect.ts
@@ -2,7 +2,8 @@ import mongoose from "mongoose";
 
 const connectDb= async()=>{
     try{
-        const db= await mongoose.connect('mongodb://localhost:27017/blog')
+        //@ts-ignore
+        const db= await mongoose.connect(process.env.MONGO_URI)
         console.log('Connected to DB');
         
     }catch(err){
