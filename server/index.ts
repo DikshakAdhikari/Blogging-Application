4af00659308315454b40a3671a410d1e548e0b92
hunktuck <ddikshakk@gmail.com>
2024-02-25 01:13:37 +0530

    mongo

--- server/index.ts
+++ server/index.ts
@@ -1,16 +1,16 @@
 import express from 'express'
 import dotenv from 'dotenv'
-import connectDb from './connection/connect'
 import userRouter from './routes/user'
 import cookieParser from 'cookie-parser'
 import blogRouter from './routes/blog'
 import cors from 'cors'
 import commentRouter from './routes/comment'
+import { mongooseConnect } from './connection/connect'
 dotenv.config()
 
 
 const app= express()
-connectDb()
+mongooseConnect()
 app.use(cors({
     origin: 'http://localhost:3000', 
     credentials: true, 
