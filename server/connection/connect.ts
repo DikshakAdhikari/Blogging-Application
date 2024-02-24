4ee2a6d28fecb1e28fdfc06cc37909f99459a326
hunktuck <ddikshakk@gmail.com>
2024-02-25 02:11:14 +0530

    mon

--- server/connection/connect.ts
+++ server/connection/connect.ts
@@ -2,7 +2,7 @@ import mongoose from "mongoose"
 export const mongooseConnect = async()=> {
     try{
         if(process.env.MONGO_URI){
-       await mongoose.connect("mongodb+srv://ddikshakk:galatHai@cluster0.p9bcrjv.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0")
+       await mongoose.connect(process.env.MONGO_URI)
        console.log('Connected Successfully');
         }
        
