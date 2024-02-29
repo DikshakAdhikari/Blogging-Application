031b0aa5a012f77e766215bd198479e8e705f966
hunktuck <ddikshakk@gmail.com>
2024-02-29 12:55:35 +0530

    hey

--- server/connection/connect.ts
+++ server/connection/connect.ts
@@ -2,13 +2,9 @@ import mongoose from "mongoose"
 export const mongooseConnect = async()=> {
     try{
         if(process.env.DATABASE_URL){
-            console.log(process.env.DATABASE_URL);
             
        await mongoose.connect(process.env.DATABASE_URL)
        console.log('Connected Successfully');
-        }else{
-            console.log("MONOOOOOO");
-            
         }
        
     }catch(err){
